725a8e969289e6da64a8af3f4937a5c2da43f380
Abhishek Ranjan <ranjanabhi3158@gmail.com>
2026-08-08 14:40:00 +0530

    locally working

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.SERVER_PORT;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
