e09d67ef4bbdb0f6aca3393149786f1a3cb47f0b
Abhishek Ranjan <ranjanabhi3158@gmail.com>
2026-08-11 10:49:57 +0530

    locally working mild errors

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { required: true, type: String, unique: true },
-    credits: { default: 500, type: Number },
+    credits: { default: 100, type: Number },
   },
   { timestamps: true },
 );
